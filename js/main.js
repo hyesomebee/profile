4b5281c62c9592f1580f1fe20d699c8ddf21a579
hyesomebee <hyesome111@gmail.com>
2023-11-28 12:45:31 +0900

    first update

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+
+    // let count = 0
+
+    // setInterval(function(){
+
+    //     count++
+    //     if(count>1){
+    //         count=0
+    //     }
+       
+    //     $(".train").css("transform",`translateY(${-50*count}%)`)
+
+    // },2000)
+})
